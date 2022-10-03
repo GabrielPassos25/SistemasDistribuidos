380419357ed506090d5a0b79e22059b415fcd67c
Gabriel Urano <gabrielurano30@alu.ufc.br>
2022-10-03 16:35:27 -0300

    Adiciona validação de CORS na API

--- Trabalho03/gateway/src/server/instance.py
+++ Trabalho03/gateway/src/server/instance.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 class Server():
     def __init__(self, ):
         self.app = Flask(__name__)
+        CORS(self.app)
     
     def run(self, ):
         self.app.run(
